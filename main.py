167ece164026376e16530ccdc0f4360bbbdc1723
silouane <s.de.reboul@gmail.com>
2025-04-07 00:37:56 +0200

    bug corrections

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-import sys 
+import sys
 sys.path.append('.')
-from opf650 import * 
-# import os 
-import numpy as np 
-import time 
+from opf650 import *
 
+import os
 
-# physical data 
-sail_angle = 70 # in degrees. I guess around 10 au près, 60 au portant ? 
-u_wind = np.array([3, 6])# 1 noeud = 0.514 m/s, 15 noeud~7m/s
-u_wind = np.array([-10, 0])# pure upwind
-u_wind = np.array([-1.736481776669303, 9.84807753012208])# best upwind
+import numpy as np
+import time
+
+# physical data
+
+sail_angle = 70 # in degrees. I guess around 10 au près, 60 au portant ?
+
+sail_angle = 45 # in degrees. I guess around 10 au près, 60 au portant ?
+
+u_wind = np.array([3, 6]) # 1 noeud = 0.514 m/s, 15 noeud~7m/s
+u_wind = np.array([-10, 0]) # pure upwind, 180
+u_wind = np.array([-1.736481776669303, 9.84807753012208]) # best upwind, 100
+# u_wind = np.array([1.736481776669303, 9.84807753012208]) # 80
+# u_wind = np.array([5, 8.660254037844386]) # 60
 boat_length=6.5
 
-# parameters 
+# parameters
+
 drag_file = '650_m1/postProcessing/forces/0/force.dat'
 controlDict_0_file = '650_basecase/system/controlDict.m0'
 controlDict_1_file = '650_basecase/system/controlDict.m1'
 u_file = '650_basecase/my0/U'
 opf_script = './go.sh'
 max_iter = 10
-threshold = 0.01 # convergence criterion 
+threshold = 0.01 # convergence criterion
 relax_factor_min = 0.2
-relax_factor_max = 0.8
-M0_ftt_length = 5 # initial run duration, to get an initial condition
-M1_ftt_length = 25 # run full duration
-M1_ftt_length = 20 # run discarded initialisation duration 
+relax_factor_max = 0.6
+M0_ftt_length = 15 # initial run duration, to get an initial condition
+M1_ftt_length = 75 # run full duration
+M1_ftt_length = 50 # run discarded initialisation duration
 
-# processing 
+# processing
+
+converged = False
 u_boat = np.zeros(max_iter+1)
-u_boat[0] = 5
+T_all = np.zeros(max_iter+1)
+D_all = np.zeros(max_iter+1)
+# u_boat[0] = 7.4
+# u_boat[0] = 6.8
+u_boat[0] = estimate_initial_velocity(u_wind, verbose=False, plot_res=False)
 relax_factors = np.linspace(0,1,max_iter)*(relax_factor_max-relax_factor_min)+relax_factor_min
 
 t0 = time.time()
@@ -40,17 +54,20 @@ for i_iter in np.arange(max_iter):
     if u_boat[i_iter] > 0.001:
         ftt = boat_length/u_boat[i_iter]
     else:
-        ftt = 1
+        print('Warning, very slow boat')
+        ftt = 0.1
     a = int(np.ceil(M0_ftt_length*ftt))
     b = int(np.ceil((M1_ftt_length+i_iter/2)*ftt))
-    u_app = compute_app_speed(np.array([u_boat[i_iter], 0]), u_wind)
+    u_app = compute_app_speed(np.array([-u_boat[i_iter], 0]), u_wind)
     # print(f"endtime = {a} and {b}")
     edit_controlDict(controlDict_0_file, endTime=a)
     edit_controlDict(controlDict_1_file, endTime=b)
     lauch_computation(opf_script)
     D = compute_average_drag(drag_file, skip=a, plot_res=False)
-    wind_force = compute_wind_force(u_app, sail_angle, verbose=True)
+    D_all[i_iter] = D
+    wind_force = compute_wind_force(u_app, verbose=False, plot_res=False)
     T = -wind_force[0]
+    T_all[i_iter] = T
     print(f'---> u_app = {u_app} ; ')
     print(f'---> Wind thrust T = {T} ; water drad D = {D}')
     print(f'---> Iteration completed in {time.time()-t1} s')
@@ -59,13 +76,29 @@ for i_iter in np.arange(max_iter):
     t1 = time.time()
     if np.isnan(u_boat[i_iter+1]):
         print(f'NaN encountered. Exiting')
-    print(f"u : {u_boat}")
+        print(f"u : {u_boat}")
     if (abs(u_boat[i_iter+1]-u_boat[i_iter]))/u_boat[i_iter] < threshold:
         print('---> Convergence reached. Exiting the loop')
+        converged = True
         break
 
 print(f"{i_iter+1} iterations done in {time.time()-t0} ")
 
+
+# Result export and plotting 
+
+filename = 'iterative_process_result'
+if converged:
+    description = f'iter u_b, T, D\n# Process converged in {i_iter-1} iterations\n# u_w={u_wind}'
+else:
+    description = f'iter u_b, T, D\n# Process did not converge\n"# u_w={u_wind}'
+matrix = np.concatenate([np.reshape(np.arange(max_iter+1), [max_iter+1, 1]), np.reshape(u_boat, [max_iter+1, 1]), np.reshape(T_all, [max_iter+1, 1]), np.reshape(D_all, [max_iter+1, 1])], axis=1)
+export_latex_curves(filename, matrix, description, verbose=True)
+
+filename = '650_m1/postProcessing/rigidBodyMotionDisplacement/tq'
+undersample_file(filename)
+filename = '650_m1/postProcessing/forces/0/force.dat'
+undersample_file(filename)
 
 
 fig, ax=plt.subplots(1, dpi=150)
@@ -75,3 +108,16 @@ ax.set_ylabel('Velocity amplitude')
 ax.set_xlabel('Iteration')
 
 plt.show()
+
+# rho = 1000
+# Sp = 0.9
+# Spp = 8.64
+
+# u = 6.5
+# Dp = 2700 # col 5 
+# Dpp = 900 # col 8
+
+# Cdp = 2*Dp/(rho * u**2 * Sp)
+# Cdpp = 2*Dpp/(rho * u**2 * Spp)
+
+# print(f'u = {u}, Cdp = {Cdp}, Cdpp = {Cdpp}')
--- opf650.py
+++ opf650.py
@@ -1,9 +1,8 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-# import sys 
-import os 
-import subprocess 
-import fileinput 
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import subprocess
+import fileinput
 
 
 
@@ -22,6 +21,16 @@ def compute_average_drag(filename, skip=0, plot_res=False):
         # print(len(drag))
     return np.mean(drag)
 
+def estimate_drag(u):
+    rho = 1000
+    Sp = 0.9 # S' = surface projetée dans un plan normal à x
+    Spp = 8.64 # S'' = surface mouillée, calculée avec integrate alpha.water sur la coque dans paraview
+    Cdp = 0.09
+    Dp = 1/2 * rho * u**2 * Sp * Cdp 
+    Cdpp = 0.003
+    Dpp = 1/2 * rho * u**2 * Spp * Cdpp 
+    return Dp+Dpp
+
 def write_u_file(filename='U', u_x=0, u_y=0, u_z=0, verbose=True):
     if verbose:
         print(f'writing U of amplitude {np.linalg.norm([u_x, u_y, u_z])} into {filename}')
@@ -35,11 +44,11 @@ def write_u_file(filename='U', u_x=0, u_y=0, u_z=0, verbose=True):
     return True
 
 def edit_controlDict(filename, endTime=1):
-    with fileinput.FileInput(filename, inplace = True, backup ='.bak') as f: 
-        for line in f: 
-            if "endTime" in line[0:10]: 
-                print(f"endTime         {endTime};", end ='\n') 
-            else: 
+    with fileinput.FileInput(filename, inplace = True, backup ='.bak') as f:
+        for line in f:
+            if "endTime" in line[0:10]:
+                print(f"endTime         {endTime};", end ='\n')
+            else:
                 print(line, end ='')
 
 def lauch_computation(opf_script):
@@ -55,12 +64,12 @@ def compute_app_speed(u_boat, u_wind, verbose=True):
 
 
 
-    # sail angle, camber en deg par rapport  l'axe du bateau. cf figure du rapport. 
+    # sail angle, camber en deg par rapport  l'axe du bateau. cf figure du rapport.
     # en upwind, la voile marche comme un profil aéro
-    # en downwind, c'est le drag qui pousse. Pour savoir dans quelle config on est, on calcule les deux et on regarde la plus grande. 
+    # en downwind, c'est le drag qui pousse. Pour savoir dans quelle config on est, on calcule les deux et on regarde la plus grande.
 
 
-def compute_wind_force(u_app, sail_angle, verbose=True, plot_res=False):
+def compute_wind_force_at_angle(u_app, sail_angle, verbose=True, plot_res=False):
     rho=1.2
     S = 34 # en m2
     camber = 20
@@ -69,24 +78,27 @@ def compute_wind_force(u_app, sail_angle, verbose=True, plot_res=False):
     camber *= np.pi/180
     sail = np.array([np.cos(sail_angle), np.sin(sail_angle)])
     rotation_mat = np.array([[np.cos(sail_angle), -np.sin(sail_angle)], [np.sin(sail_angle), np.cos(sail_angle)]])
-    print(f"rotation mat = {rotation_mat}")
-    # upwind hypothesis 
-    Cl = 0.8   # réglage au près - upwind 
+    # if verbose:
+    #     print(f"rotation mat = {rotation_mat}")
+    # upwind hypothesis
+    Cl = 0.8   # réglage au près - upwind
     Cd = 0.02  # réglage au près - upwind
     u = u_app@sail.T
+    if u < 0:
+        u = 0
     Fl_upwind = 0.5*rho*u**2*S*Cl
     Fd_upwind = 0.5*rho*u**2*S*Cd
     F_upwind = rotation_mat@np.array([Fd_upwind, Fl_upwind]).T
     F_upwind_pas_rot = np.array([Fd_upwind, Fl_upwind])
     if verbose:
         print(f'---> Upwind mode, u_app = {u_app}, u*sail = {u}, F = {F_upwind}')
-    # pas upwind hypothesis 
+    # pas upwind hypothesis
     bullshit_coeff = 0.5
     sail = np.array([-np.sin(sail_angle), np.cos(sail_angle)])
     u = u_app@sail.T
-    stagnation_pressure = 0.5*rho*u**2*bullshit_coeff 
+    stagnation_pressure = 0.5*rho*u**2*bullshit_coeff
     Fl_downwind = 0
-    Fd_downwind = S * stagnation_pressure  
+    Fd_downwind = S * stagnation_pressure
     F_downwind = rotation_mat@np.array([Fl_downwind, Fd_downwind]).T
     if verbose:
         print(f'---> Downwind mode, u_app = {u_app}, u*sail = {u}, F = {F_downwind}')
@@ -95,8 +107,8 @@ def compute_wind_force(u_app, sail_angle, verbose=True, plot_res=False):
         F = F_downwind
     if verbose:
         print(f"F = {F}")
-    if F[0] > 0:
-        print(f"WARNING The wind thrust is not pushing forward")
+    # if F[0] > 0:
+    #     print(f"WARNING The wind thrust is not pushing forward")
     if plot_res:
         fig, ax = plt.subplots(1, dpi=100)
         ax.set_xlim(-1, 1)
@@ -129,16 +141,28 @@ def compute_wind_force(u_app, sail_angle, verbose=True, plot_res=False):
         plt.show()
     return F
 
-# compute_wind_force(u_app, sail_angle, verbose=verbose, plot_res=plot_res)
+def compute_wind_force(u_app, verbose=True, plot_res=False):
+    F_max = 0
+    for sail_angle in range(0, 90, 1):
+        F = compute_wind_force_at_angle(u_app, sail_angle, verbose=False)
+        if np.linalg.norm(F) > F_max:
+            F_max = np.linalg.norm(F)
+            sail_angle_max = sail_angle
+            F_max_vect = F
+    if verbose:
+        print(f"Maximal force {F_max} for sail angle {sail_angle_max}")
+        F = compute_wind_force_at_angle(u_app, sail_angle, verbose=verbose, plot_res=plot_res)
+    return F_max_vect
+
 
 def remove_alternate_lines(file_path):
     # Lire toutes les lignes du fichier
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-    
+
     # Garder une ligne sur deux
     filtered_lines = lines[::2]
-    
+
     # Réécrire le fichier avec les lignes filtrées
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(filtered_lines)
@@ -147,15 +171,75 @@ def undersample_file(file_path, N=200):
     # Lire toutes les lignes du fichier
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-    
+
     # Vérifier si le fichier est plus petit que N lignes
     if len(lines) <= N:
         return
-    
+
     # Sélectionner N lignes réparties uniformément
     indices = np.linspace(0, len(lines) - 1, N, dtype=int)
     filtered_lines = [lines[i] for i in indices]
-    
+
     # Réécrire le fichier avec les lignes filtrées
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(filtered_lines)
+
+
+def export_latex_curves(filename, matrix, description, verbose=True):
+    # filename, matrix, description
+    if verbose:
+        print(f"Exporting {filename} with description {description}: \n{matrix}")
+    with open(filename, 'w') as dataFile:
+        dataFile.write('# {}\n'.format(description))
+        for row in matrix:
+            dataFile.write(' '.join([str(a) for a in row]) + '\n')
+
+
+def estimate_initial_velocity(u_wind, verbose=True, plot_res=True):
+    converged = False
+    threshold = 0.01
+    max_iter = 100
+    u_boat = np.zeros(max_iter+1)
+    T_all = np.zeros(max_iter+1)
+    D_all = np.zeros(max_iter+1)
+    u_boat[0] = 1
+    relax_factor_max = 0.98
+    relax_factor_min = 0.2
+    relax_factors = np.linspace(0,1,max_iter)*(relax_factor_max-relax_factor_min)+relax_factor_min
+    for i_iter in np.arange(max_iter):
+        print(f'Iteration {i_iter} ; u = {u_boat[i_iter]}')
+        u_app = compute_app_speed(np.array([-u_boat[i_iter], 0]), u_wind)
+        D = estimate_drag(u_boat[i_iter])
+        D_all[i_iter] = D
+        wind_force = compute_wind_force(u_app, verbose=False, plot_res=False)
+        T = wind_force[0]
+        T_all[i_iter] = T
+        print(f'---> u_app = {u_app} ; ')
+        print(f'---> Wind thrust T = {T} ; water drad D = {D}')
+        relax_factor = relax_factors[i_iter]
+        u_boat[i_iter+1] = relax_factor*u_boat[i_iter]*-T/D+(1-relax_factor)*u_boat[i_iter]
+        if np.isnan(u_boat[i_iter+1]):
+            print(f'NaN encountered. Exiting')
+            print(f"u : {u_boat}")
+        if (abs(u_boat[i_iter+1]-u_boat[i_iter]))/u_boat[i_iter] < threshold:
+            print('---> Convergence reached. Exiting the loop')
+            converged = True
+            break
+    u_boat = u_boat[0:i_iter+1]
+    D_all = D_all[0:i_iter+1]
+    T_all = T_all[0:i_iter+1]
+    if plot_res:
+        fig, ax=plt.subplots(1,2, dpi=150)
+        ax[0].plot(np.arange(len(u_boat)), u_boat)
+        ax[0].set_title('velocity boundary condition')
+        ax[0].set_ylabel('Velocity amplitude')
+        ax[0].set_xlabel('Iteration')
+        ax[1].plot(np.arange(len(u_boat)), T_all, label='T')
+        ax[1].plot(np.arange(len(u_boat)), -D_all, label='-D')
+        ax[1].set_title('Forces')
+        ax[1].set_ylabel('Forces')
+        ax[1].set_xlabel('Iteration')
+        plt.show()
+    return u_boat[-1]
+
+estimate_initial_velocity(u_wind)
